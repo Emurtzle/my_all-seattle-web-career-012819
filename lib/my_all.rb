0701b99f2986b803d289db13b33f7ef33bf9065e
Emily Wagner <emilywagner598@gmail.com>
2019-01-30 01:21:02 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,11 @@ def my_all?(collection)
   while < collection.length
     block_return_values << yield(collection[i])
 
-
     i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
